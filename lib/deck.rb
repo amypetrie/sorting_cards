3dd10671465022f104286b18f801d85d1f151a83
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-08-21 12:05:49 -0600

    adds updated deck class and test files

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,4 +10,19 @@ attr_reader :cards
     cards.to_ary.count
   end
 
+  def sort
+    value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+    suit = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    sorted = []
+    cards.each do |card|
+      new_index = cards.rindex(card) + 1
+      if card.value < cards[new_index].value.to_s
+        sorted << card
+      else
+        break
+      end
+    end
+    sorted
+  end
+
 end
